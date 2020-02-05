06313e81d547ebe3f4f1367bcf8b85e3f8e283c9
edvinpontuslarsson <el223na@student.lnu.se>
2020-02-05 13:08:12 +0100

    gets related questions

--- src/requests/stackExchange.ts
+++ src/requests/stackExchange.ts
@@ -1,13 +1,31 @@
 import fetch from 'node-fetch'
-import * as fs from 'fs'
 
 export const doStackExchangeRequests = (): void => {
-  soInfo()
+  stackOverflowInfo()
+  relatedQuestionsSO()
 }
 
-async function soInfo(): Promise<void> {
+async function stackOverflowInfo(): Promise<void> {
   const uri =
     'https://api.stackexchange.com/2.2/info?site=stackoverflow'
+
+  const res = await fetch(uri)
+
+  const body = await res.text()
+  console.log(body)
+}
+
+// TODO have just one funciton for get requests
+
+/**
+ * Related questions to these 3 questions:
+ * https://stackoverflow.com/questions/60075228/exchange-different-arrays-elements-in-ruby
+ * https://stackoverflow.com/questions/60075237/best-way-to-handle-an-account-linking-verification-system
+ * https://stackoverflow.com/questions/57496313/execution-failed-for-task-appmergedebugresources-com-android-builder-interna
+ */
+async function relatedQuestionsSO(): Promise<void> {
+  const uri =
+    'https://api.stackexchange.com/2.2/questions/60075228;60075237;57496313/related?order=desc&sort=activity&site=stackoverflow'
 
   const res = await fetch(uri)
 

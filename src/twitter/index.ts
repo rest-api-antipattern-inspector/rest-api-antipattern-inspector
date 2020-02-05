da32528d3b68b26e5206f64b37a8d1ff4d69a34e
Jesper Hägglund <jeppan875@gmail.com>
2020-02-05 15:25:40 -0300

    Twitter integration (#4)
    * connect to twitter api
    
    * twitter api
    
    * removed unused
    
    * ts
    
    * ts fix

---
+++ src/twitter/index.ts
@@ -0,0 +1,33 @@
+require('dotenv').config()
+import Twit from 'twit'
+import endpoints from './endpoints'
+
+interface Endpoint {
+  readonly method: string
+  readonly url: string
+  readonly params: object
+}
+
+const twitterClient = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_API_KEY || '',
+  consumer_secret:
+    process.env.TWITTER_CONSUMER_API_SECRET || '',
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret:
+    process.env.TWITTER_ACCESS_TOKEN_SECRET,
+})
+
+endpoints.forEach((endpoint: Endpoint) => {
+  try {
+    twitterClient.get(
+      endpoint.url,
+      endpoint.params,
+      function(err: any, data: any, response: any) {
+        console.log(data)
+        //console.log(response)
+      }
+    )
+  } catch (e) {
+    console.log(e)
+  }
+})

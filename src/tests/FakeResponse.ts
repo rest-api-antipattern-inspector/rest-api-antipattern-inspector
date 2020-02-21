a8f9472eaf20ac5a0f2d028445c1f30636664150
edvinpontuslarsson <el223na@student.lnu.se>
2020-02-21 13:48:00 +0100

    puts fake response in separate file

---
+++ src/tests/FakeResponse.ts
@@ -0,0 +1,43 @@
+import IResponse from '../interfaces/IResponse'
+
+export default class FakeResponse {
+  status: number
+  textContent: string
+  headers: string[]
+  responseStub: IResponse
+
+  constructor(
+    status: number,
+    textContent: string,
+    headers: string[]
+  ) {
+    this.status = status
+    this.textContent = textContent
+    this.headers = headers
+
+    this.responseStub = {
+      status: this.status,
+      text: this.textReturner,
+      headers: {
+        has: this.hasHeader,
+        get: this.getHeader,
+      },
+    }
+  }
+
+  textReturner(): Promise<string> {
+    return new Promise((resolve) => {
+      resolve(this.textContent)
+    })
+  }
+
+  hasHeader(name: string): boolean {
+    return this.headers.includes(name)
+  }
+
+  getHeader(name: string): string | null {
+    return (
+      this.headers.find((item) => item === name) || null
+    )
+  }
+}

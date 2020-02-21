37cd79dcb65af8f9783b2aa9e4598c87142abae2
edvinpontuslarsson <el223na@student.lnu.se>
2020-02-21 13:53:58 +0100

    adds a test function

--- src/tests/designAntipatternDetectors.test.ts
+++ src/tests/designAntipatternDetectors.test.ts
@@ -8,5 +8,15 @@ import {
 } from '../lib/designAntipatternsDetectors'
 import FakeResponse from './FakeResponse'
 
-const responseStub = new FakeResponse(0, '', [])
-  .responseStub
+describe('Tests of Design Antipattern Detections', () => {
+  describe('Tests of Ignoring Status Code Detection', () => {
+    it('HTTP Post with status code 200 should detect antipattern', () => {
+      const responseStub = new FakeResponse(200, '', [])
+        .responseStub
+
+      expect(
+        isIgnoringStatusCode(responseStub, 'post')
+      ).toBeTruthy()
+    })
+  })
+})

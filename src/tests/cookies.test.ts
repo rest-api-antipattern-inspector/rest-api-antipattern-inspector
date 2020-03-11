8b57b9e6df48d8318687c0c2414f752acc5d56e3
edvinpontuslarsson <el223na@student.lnu.se>
2020-03-11 18:19:01 +0100

    tests added for misusing cookies

--- src/tests/cookies.test.ts
+++ src/tests/cookies.test.ts
@@ -8,3 +8,25 @@ test('Misusing Cookies: true, headers contain set-cookie', () => {
 
   expect(isMisusingCookies(responseStub)).toBeTruthy()
 })
+
+test('Misusing Cookies: true, headers contain cookie', () => {
+  const responseStub = new FakeResponse(200, '', ['cookie'])
+    .responseStub
+
+  expect(isMisusingCookies(responseStub)).toBeTruthy()
+})
+
+test('Misusing Cookies: false, headers does not contain cookie or set-cookie', () => {
+  const responseStub = new FakeResponse(200, '', [
+    'Content-Length',
+  ]).responseStub
+
+  expect(isMisusingCookies(responseStub)).toBeFalsy()
+})
+
+test('Misusing Cookies: false, empty headers', () => {
+  const responseStub = new FakeResponse(200, '', [])
+    .responseStub
+
+  expect(isMisusingCookies(responseStub)).toBeFalsy()
+})

67a54b56d1c009e304a9bf16f9c9396c8a04bbd9
Jesper Hägglund <jesper.hagglund@trmedia.se>
2020-03-23 17:15:40 +0100

    twitter

--- src/requests/twitter/endpoints.ts
+++ src/requests/twitter/endpoints.ts
@@ -1,20 +1,59 @@
+import {
+  SCREEN_NAME,
+  COUNT,
+  SCREEN_NAME_2,
+} from './constants'
+import { GET, POST } from '../../lib/constants'
+
 export default [
   {
-    url:
-      'statuses/user_timeline.json?screen_name=Carl_Gustav_XVI&count=1',
-    method: 'get',
-    statusCode: 200,
+    url: 'statuses/user_timeline',
+    method: GET,
+    params: { screen_name: SCREEN_NAME_2, count: COUNT },
   },
   {
-    url:
-      'followers/ids.json?screen_name=Carl_Gustav_XVI&count=1',
-    method: 'get',
-    statusCode: 200,
+    url: 'followers/ids',
+    method: GET,
+    params: { screen_name: SCREEN_NAME, count: COUNT },
   },
   {
-    url:
-      'followers/list.json?screen_name=Carl_Gustav_XVI&count=1',
-    method: 'get',
-    statusCode: 200,
+    url: 'followers/list',
+    method: GET,
+    params: { screen_name: SCREEN_NAME, count: COUNT },
+  },
+  {
+    url: 'friends/ids',
+    method: GET,
+    params: { screen_name: SCREEN_NAME, count: COUNT },
+  },
+  {
+    url: 'friends/list',
+    method: GET,
+    params: { screen_name: SCREEN_NAME, count: COUNT },
+  },
+  {
+    url: 'friendships/incoming',
+    method: GET,
+    params: { screen_name: SCREEN_NAME, count: COUNT },
+  },
+  {
+    url: `friendships/lookup`,
+    method: GET,
+    params: { screen_name: SCREEN_NAME, count: COUNT },
+  },
+  {
+    url: `friendships/no_retweets/ids`,
+    method: GET,
+    params: { screen_name: SCREEN_NAME, count: COUNT },
+  },
+  {
+    url: `friendships/outgoing`,
+    method: GET,
+    params: { screen_name: SCREEN_NAME, count: COUNT },
+  },
+  {
+    url: `friendships/show`,
+    method: GET,
+    params: { screen_name: SCREEN_NAME, count: COUNT },
   },
 ]
--- src/requests/twitter/index.ts
+++ src/requests/twitter/index.ts
@@ -1,30 +1,28 @@
-import fetch from 'node-fetch'
 import endpoints from './endpoints'
 import { storeResponseMeta } from '../../lib/storeMeta'
+import Twit from 'twit'
 
-const BASE_URL = 'https://api.twitter.com/1.1/'
+var T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_API_KEY || '',
+  consumer_secret:
+    process.env.TWITTER_CONSUMER_API_SECRET || '',
+  access_token: process.env.TWITTER_ACCESS_TOKEN || '',
+  access_token_secret:
+    process.env.TWITTER_ACCESS_TOKEN_SECRET || '',
+})
 
 interface Endpoint {
   readonly method: string
   readonly url: string
-  readonly statusCode: number
+  readonly params: object
 }
 
 export default () =>
   endpoints.forEach(async (endpoint: Endpoint) => {
-    const response = await fetch(
-      `${BASE_URL}${endpoint.url}`,
-      {
-        method: endpoint.method,
-        headers: {
-          Authorization:
-            'Bearer ' + process.env.TWITTER_TOKEN,
-        },
-      }
-    )
-    storeResponseMeta(
+    const response = await T.get(
       endpoint.url,
-      response,
-      endpoint.method
+      endpoint.params
     )
+
+    console.log(response)
   })

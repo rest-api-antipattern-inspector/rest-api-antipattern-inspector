37f9964bdf3cec5858cef902ff75a71674da8011
Jesper Hägglund <jesper.hagglund@trmedia.se>
2020-03-25 08:36:27 +0100

    twitter-api

--- src/requests/twitter/endpoints.ts
+++ src/requests/twitter/endpoints.ts
@@ -1,16 +1,143 @@
+// https://developer.twitter.com/en/docs/api-reference-index
+
 import {
   SCREEN_NAME,
   COUNT,
   SCREEN_NAME_2,
+  QUERY,
+  LIST_SLUG,
 } from './constants'
 import { GET, POST } from '../../lib/constants'
 
-export default [
+export const postEndpoint1 = [
+  // Create and manage lists
   {
-    url: 'statuses/user_timeline',
-    method: GET,
+    url: `lists/create`,
+    method: POST,
+    params: { name: LIST_SLUG },
+  },
+  // Follow, search, and get users
+  {
+    url: `friendships/create`,
+    method: POST,
     params: { screen_name: SCREEN_NAME_2, count: COUNT },
   },
+  {
+    url: `friendships/update`,
+    method: POST,
+    params: { screen_name: SCREEN_NAME_2, count: COUNT },
+  },
+  // Mute, block, and report users
+  {
+    url: 'blocks/create',
+    method: POST,
+    params: { screen_name: SCREEN_NAME_2 },
+  },
+
+  {
+    url: 'mutes/users/create',
+    method: POST,
+    params: { screen_name: SCREEN_NAME_2 },
+  },
+]
+
+export const postEndpoint2 = [
+  // Create and manage lists
+  {
+    url: `lists/members/create`,
+    method: POST,
+    params: {
+      slug: LIST_SLUG,
+      owner_screen_name: SCREEN_NAME,
+      screen_name: SCREEN_NAME,
+    },
+  },
+  {
+    url: `lists/subscribers/create`,
+    method: POST,
+    params: {
+      slug: LIST_SLUG,
+      owner_screen_name: SCREEN_NAME,
+    },
+  },
+]
+
+export const getEndpoints = [
+  //Create and manage lists
+  {
+    url: 'lists/list',
+    method: GET,
+    params: { screen_name: SCREEN_NAME },
+  },
+  {
+    url: 'lists/members',
+    method: GET,
+    params: {
+      slug: LIST_SLUG,
+      owner_screen_name: SCREEN_NAME,
+    },
+  },
+  {
+    url: 'lists/members/show',
+    method: GET,
+    params: {
+      slug: LIST_SLUG,
+      owner_screen_name: SCREEN_NAME,
+      screen_name: SCREEN_NAME,
+    },
+  },
+  {
+    url: 'lists/memberships',
+    method: GET,
+    params: { screen_name: SCREEN_NAME },
+  },
+  {
+    url: 'lists/ownerships',
+    method: GET,
+    params: { screen_name: SCREEN_NAME },
+  },
+  {
+    url: 'lists/show',
+    method: GET,
+    params: {
+      slug: LIST_SLUG,
+      owner_screen_name: SCREEN_NAME,
+    },
+  },
+  {
+    url: 'lists/statuses',
+    method: GET,
+    params: {
+      slug: LIST_SLUG,
+      owner_screen_name: SCREEN_NAME,
+    },
+  },
+  {
+    url: 'lists/subscribers',
+    method: GET,
+    params: {
+      slug: LIST_SLUG,
+      owner_screen_name: SCREEN_NAME,
+    },
+  },
+  {
+    url: 'lists/subscribers/show',
+    method: GET,
+    params: {
+      slug: LIST_SLUG,
+      owner_screen_name: SCREEN_NAME,
+      screen_name: SCREEN_NAME,
+    },
+  },
+  {
+    url: 'lists/subscriptions',
+    method: GET,
+    params: {
+      slug: LIST_SLUG,
+      owner_screen_name: SCREEN_NAME,
+    },
+  },
+  // Follow, search, and get users
   {
     url: 'followers/ids',
     method: GET,
@@ -54,6 +181,118 @@ export default [
   {
     url: `friendships/show`,
     method: GET,
+    params: {
+      target_screen_name: SCREEN_NAME_2,
+      count: COUNT,
+    },
+  },
+  {
+    url: `users/lookup`,
+    method: GET,
     params: { screen_name: SCREEN_NAME, count: COUNT },
+  },
+  {
+    url: `users/search`,
+    method: GET,
+    params: {
+      screen_name: SCREEN_NAME_2,
+      count: COUNT,
+      q: QUERY,
+    },
+  },
+  {
+    url: `users/show`,
+    method: GET,
+    params: {
+      screen_name: SCREEN_NAME_2,
+      count: COUNT,
+    },
+  },
+  // Get Tweet timelines
+  {
+    url: 'statuses/home_timeline',
+    method: GET,
+    params: { screen_name: SCREEN_NAME, count: COUNT },
+  },
+  {
+    url: 'statuses/mentions_timeline',
+    method: GET,
+    params: { screen_name: SCREEN_NAME, count: COUNT },
+  },
+  {
+    url: 'statuses/user_timeline',
+    method: GET,
+    params: { screen_name: SCREEN_NAME, count: COUNT },
+  },
+  // Mute, block, and report users
+  {
+    url: 'blocks/ids',
+    method: GET,
+    params: { count: COUNT },
+  },
+  {
+    url: 'blocks/list',
+    method: GET,
+    params: { count: COUNT },
+  },
+  {
+    url: 'mutes/users/ids',
+    method: GET,
+    params: { count: COUNT },
+  },
+  {
+    url: 'mutes/users/list',
+    method: GET,
+    params: { count: COUNT },
+  },
+]
+
+export const deleteEndpoint1 = [
+  // Create and manage lists
+  {
+    url: `lists/members/destroy`,
+    method: POST,
+    params: {
+      slug: LIST_SLUG,
+      owner_screen_name: SCREEN_NAME,
+      screen_name: SCREEN_NAME,
+    },
+  },
+  {
+    url: `lists/subscribers/destroy`,
+    method: POST,
+    params: {
+      slug: LIST_SLUG,
+      owner_screen_name: SCREEN_NAME,
+    },
+  },
+]
+
+export const deleteEndpoint2 = [
+  // Create and manage lists
+  {
+    url: `lists/destroy`,
+    method: POST,
+    params: {
+      slug: LIST_SLUG,
+      owner_screen_name: SCREEN_NAME,
+    },
+  },
+  // Follow, search, and get users
+  {
+    url: `friendships/destroy`,
+    method: POST,
+    params: { screen_name: SCREEN_NAME_2, count: COUNT },
+  },
+  // Mute, block, and report users
+  {
+    url: 'blocks/destroy',
+    method: POST,
+    params: { screen_name: SCREEN_NAME_2 },
+  },
+  {
+    url: 'mutes/users/destroy',
+    method: POST,
+    params: { screen_name: SCREEN_NAME_2 },
   },
 ]

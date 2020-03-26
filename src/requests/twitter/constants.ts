fc4a462eb13575c6c5b39184ab1ca1bc4d16fe0f
edvinpontuslarsson <el223na@student.lnu.se>
2020-03-26 14:35:27 +0100

    updates env stuff

--- src/requests/twitter/constants.ts
+++ src/requests/twitter/constants.ts
@@ -1,6 +1,10 @@
-export const SCREEN_NAME = 'JesperHagglund' // authorized
-export const SCREEN_NAME_2 = 'jeppan875'
-export const ID_2 = '1121556159197007873'
+export const SCREEN_NAME =
+  process.env.TWITTER_SCREEN_NAME || 'screen name in .env' // authorized
+export const SCREEN_NAME_2 =
+  process.env.TWITTER_SCREEN_NAME_2 ||
+  'screen name 2 in .env'
+export const ID_2 =
+  process.env.TWITTER_ID_2 || 'ID 2 in .env'
 export const COUNT = 1
 export const QUERY = 'Twitter%20API'
 export const LIST_SLUG = 'mylist'

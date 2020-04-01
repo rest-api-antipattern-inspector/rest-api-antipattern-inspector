aacccd39f644a8f76f052b56cd868060fd6d9f86
Jesper Hägglund <jesper.hagglund@trmedia.se>
2020-04-01 21:34:09 +0200

    disqus start

--- src/requests/bitly/constants.ts
+++ src/requests/bitly/constants.ts
@@ -1,2 +1,3 @@
+// you need to create a bitlink manually on their admin page
 export const ORG_ID = 'Ok3sdj3Oa5X'
 export const BITLINK = 'bit.ly/2xy1gEU'
---
+++ src/requests/disqus/constants.ts
@@ -0,0 +1,2 @@
+// need to first create a forum manullay on https://disqus.com/api/docs/forums/create/
+export const forum = ''

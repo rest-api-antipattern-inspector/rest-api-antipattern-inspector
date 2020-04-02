7ef32b3aa64b7a260a38fd102ee1f10cfbac67d8
Jesper Hägglund <jesper.hagglund@trmedia.se>
2020-04-02 22:03:49 +0200

    disqus api

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,13 @@ dotenv.config()
 import { doStackExchangeRequests } from './requests/stackExchange/stackExchange'
 import doTwitterRequests from './requests/twitter'
 import bitlyRequests from './requests/bitly'
+import disqusRequests from './requests/disqus'
 
 const APIs: any = {
   stackexchange: doStackExchangeRequests,
   twitter: doTwitterRequests,
   bitly: bitlyRequests,
+  disqus: disqusRequests,
 }
 
 // TODO create text file for Java based on responses.json
--- src/requests/disqus/index.ts
+++ src/requests/disqus/index.ts
@@ -1,36 +1,53 @@
 import { endpoints } from './endpoints'
 import { storeResponseMeta } from '../../lib/storeMeta'
 import fetch from 'node-fetch'
-const ACCESS_TOKEN = process.env.DISQUS_SECRET
-const BASE_URL = 'https://api-ssl.bitly.com/v4/'
+const DISQUS_SECRET = process.env.DISQUS_SECRET
+const DISQUS_ACCESS_TOKEN = process.env.DISQUS_ACCESS_TOKEN
+const BASE_URL = 'https://disqus.com/api/3.0/'
 
 interface Endpoint {
   readonly method: string
   readonly url: string
+  readonly params?: string
   readonly data?: object
 }
 
 export default async () => {
-  const res = await Promise.all(
-    endpoints.map((endpoint: Endpoint) => {
-      return fetch(`${BASE_URL}${endpoint.url}`, {
-        method: endpoint.method,
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${ACCESS_TOKEN}`,
-        },
-        body:
-          endpoint.data && endpoint.method !== 'GET'
-            ? JSON.stringify(endpoint.data)
-            : undefined,
-      })
+  const result = await Promise.all(
+    endpoints.map(async (endpoint: Endpoint) => {
+      const res = await fetch(
+        `${BASE_URL}${endpoint.url}?api_secret=${DISQUS_SECRET}&access_token=${DISQUS_ACCESS_TOKEN}${endpoint.params}`,
+        {
+          method: endpoint.method,
+          body:
+            endpoint.data && endpoint.method !== 'GET'
+              ? JSON.stringify(endpoint.data)
+              : undefined,
+        }
+      )
+
+      const body = await res.text()
+      const headers: any = {}
+      res.headers.forEach((value, name) => (headers[name] = value))
+      return {
+        wholeURI: res.url,
+        endpoint: endpoint.url,
+        statusCode: res.status,
+        headers,
+        body: JSON.parse(body),
+        httpMethod: endpoint.method,
+      }
     })
   )
-  console.log(res.length)
-  res.map(async (re) => {
-    const text = await re.text()
-    console.log(re.status)
-    console.log(re.url)
-  })
+  console.log(result)
+  result.map((res) =>
+    storeResponseMeta(
+      res.wholeURI,
+      res.endpoint,
+      res.statusCode,
+      res.headers,
+      res.body,
+      res.httpMethod
+    )
+  )
 }

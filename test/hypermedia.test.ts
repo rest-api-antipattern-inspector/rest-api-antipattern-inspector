6221bfd61854b891aa1420c896c985dc66a453c1
edvinpontuslarsson <el223na@student.lnu.se>
2020-04-07 16:41:53 +0200

    adds all files w. todos

---
+++ test/hypermedia.test.ts
@@ -0,0 +1,1 @@
+// TODO test that lower & uppercase versions all work
---
+++ test/selfDescriptiveness.test.ts
@@ -0,0 +1,4 @@
+// TODO loop through all standard headers, make sure they all work
+// also do them to lowercase, should still work
+
+// add some own headers, should break

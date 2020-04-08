86f6cc41c654bd0cc47f9149d57388405d7b4e2a
edvinpontuslarsson <el223na@student.lnu.se>
2020-04-08 16:17:13 +0200

    adds mime type tests

--- test/mimeType.test.ts
+++ test/mimeType.test.ts
@@ -1,11 +1,22 @@
 import { isIgnoringMIMEType } from '../src/lib/designAntipatternsDetectors'
 import MIMETypes from '../src/lib/MIMETypes'
 
-// TODO again, check that it works for both
-// Capitalized & lowercase versions of headers
-
 test('Ignoring Caching: false. Capitalized Content-Type header', () => {
   for (let mt of MIMETypes) {
     expect(isIgnoringMIMEType({ 'Content-Type': mt })).toBeFalsy()
   }
+})
+
+test('Ignoring Caching: false. lowercase content-type header', () => {
+  for (let mt of MIMETypes) {
+    expect(isIgnoringMIMEType({ 'content-type': mt })).toBeFalsy()
+  }
+})
+
+test('Ignoring Caching: true. Missing Content-Type header', () => {
+  expect(isIgnoringMIMEType({ location: 'google.com' })).toBeTruthy()
+})
+
+test('Ignoring Caching: true. Invalid MIME type', () => {
+  expect(isIgnoringMIMEType({ 'content-type': 'whatever' })).toBeTruthy()
 })

a9af0a8d458957f967e46f1b2dde14975de37a1e
edvinpontuslarsson <el223na@student.lnu.se>
2020-04-08 15:53:04 +0200

    adds a mime type test

--- test/mimeType.test.ts
+++ test/mimeType.test.ts
@@ -1,2 +1,11 @@
+import { isIgnoringMIMEType } from '../src/lib/designAntipatternsDetectors'
+import MIMETypes from '../src/lib/MIMETypes'
+
 // TODO again, check that it works for both
 // Capitalized & lowercase versions of headers
+
+test('Ignoring Caching: false. Capitalized Content-Type header', () => {
+  for (let mt of MIMETypes) {
+    expect(isIgnoringMIMEType({ 'Content-Type': mt })).toBeFalsy()
+  }
+})

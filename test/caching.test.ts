6cf87a96f39af32b5b5bd42a28cfc9f6c3df17df
edvinpontuslarsson <el223na@student.lnu.se>
2020-04-09 14:32:45 +0200

    adds todo

--- test/caching.test.ts
+++ test/caching.test.ts
@@ -1,6 +1,9 @@
 import { isIgnoringCaching } from '../src/lib/designAntipatternsDetectors'
 import { GET, POST, PUT, PATCH, DELETE } from '../src/lib/constants'
 
+// TODO mention full coverage of all branches, statements etc of detector functions in report
+// with image
+
 ///
 
 // FALSE

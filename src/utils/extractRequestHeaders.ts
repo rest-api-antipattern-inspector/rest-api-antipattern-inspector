672b4c27a82e09c3e63cace3c5cc99efc792b5a7
edvinpontuslarsson <el223na@student.lnu.se>
2020-04-18 15:21:37 +0200

    extracts headers properly

--- src/utils/extractRequestHeaders.ts
+++ src/utils/extractRequestHeaders.ts
@@ -1,7 +1,19 @@
-export default (reqHeaderText: string): string[] => {
-  const rawReqHeaders = reqHeaderText.split('\r\n')
-  rawReqHeaders.shift()
+import IHeadersObject from '../interfaces/IHeadersObject'
 
-  const reqHeaders = rawReqHeaders.filter((item) => item !== '')
-  return reqHeaders
+export default (reqHeaderText: string): IHeadersObject => {
+  const rawReqHeaderFields = reqHeaderText.split('\r\n')
+  rawReqHeaderFields.shift()
+  const reqHeaderFields = rawReqHeaderFields.filter((item) => item !== '')
+
+  const requestHeadersObj: IHeadersObject = {}
+
+  reqHeaderFields.forEach((field) => {
+    const colonIndex = field.indexOf(':')
+    const key = field.slice(0, colonIndex)
+    const value = field.slice(colonIndex + 1).replace(' ', '')
+
+    requestHeadersObj[key] = value
+  })
+
+  return requestHeadersObj
 }

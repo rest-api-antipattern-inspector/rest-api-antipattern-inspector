6d923f521cc105828b93fbcb4f621ea50b6e5359
edvinpontuslarsson <el223na@student.lnu.se>
2020-04-18 15:29:13 +0200

    stores data properly

--- src/requests/stackExchange/stackExchange.ts
+++ src/requests/stackExchange/stackExchange.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import { storeResponseMeta } from '../../data-access-layer/storeMeta'
 import { APIs } from '../../enums/APIs'
-import IHeadersObject from '../../interfaces/IHeadersObject'
 import { GET, POST, PUT, PATCH, DELETE } from '../../utils/HTTPMethods'
 import extractRequestHeaders from '../../utils/extractRequestHeaders'
 
@@ -15,35 +14,25 @@ async function stackOverflowInfo() {
 
   axios.get(uri).then((response) => {
     const reqHeaderString = response.request._header
-    const requestHeaders = extractRequestHeaders(reqHeaderString)
+    const reqHeaders = extractRequestHeaders(reqHeaderString)
 
-    console.log('stackexchange')
-    console.log(uri)
-    console.log('/2.2/info?site=stackoverflow')
+    storeResponseMeta({
+      api: APIs.stackExchange,
+      wholeURI: uri,
+      endpoint: '/2.2/info?site=stackoverflow',
 
-    console.log('GET')
-    console.log({
-      statusCode: response.status,
-      statusText: response.statusText,
+      httpMethod: GET,
+      status: {
+        statusCode: response.status,
+        statusText: response.statusText,
+      },
+
+      requestHeaders: reqHeaders,
+      responseHeaders: response.headers,
+
+      body: response.data,
     })
-
-    console.log(requestHeaders)
-    console.log(response.headers)
-
-    console.log(response.data)
   })
-
-  /*
-  storeResponseMeta(
-    APIs.stackExchange,
-    uri,
-    '/2.2/info?site=stackoverflow',
-    res.status,
-    headers,
-    body,
-    GET
-  )
-  */
 }
 
 /**
@@ -56,52 +45,26 @@ async function relatedQuestionsSO() {
   const uri =
     'https://api.stackexchange.com/2.2/questions/60075228;60075237;57496313/related?order=desc&sort=activity&site=stackoverflow'
 
-  // TODO make req & storeMeta
+  axios.get(uri).then((response) => {
+    const reqHeaderString = response.request._header
+    const reqHeaders = extractRequestHeaders(reqHeaderString)
 
-  // axios.get(uri).then((response) => {
-  //   const reqHeaderString = response.request._header
-  //   const requestHeaders = extractRequestHeaders(reqHeaderString)
+    storeResponseMeta({
+      api: APIs.stackExchange,
+      wholeURI: uri,
+      endpoint:
+        '/2.2/questions/60075228;60075237;57496313/related?order=desc&sort=activity&site=stackoverflow',
 
-  //   console.log('stackexchange')
-  //   console.log(uri)
-  //   console.log('/2.2/info?site=stackoverflow')
+      httpMethod: GET,
+      status: {
+        statusCode: response.status,
+        statusText: response.statusText,
+      },
 
-  //   console.log('GET')
-  //   console.log({
-  //     statusCode: response.status,
-  //     statusText: response.statusText,
-  //   })
+      requestHeaders: reqHeaders,
+      responseHeaders: response.headers,
 
-  //   console.log(requestHeaders)
-  //   console.log(response.headers)
-
-  //   console.log(response.data)
-  // })
-
-  /*
-  storeResponseMeta(
-    APIs.stackExchange,
-    uri,
-    '/2.2/questions/{question_ids}/related?order=desc&sort=activity&site=stackoverflow',
-    res.status,
-    headers,
-    body,
-    GET
-  )
-  */
+      body: response.data,
+    })
+  })
 }
-
-/*
-function getHeaders(res: Response): IHeadersObject {
-  const headers: IHeadersObject = {}
-  const rawHeaders = res.headers.raw()
-  const headerKeys = Object.keys(rawHeaders)
-  const headerValues = Object.values(rawHeaders)
-
-  for (let i = 0; i < headerKeys.length; i++) {
-    headers[headerKeys[i]] = headerValues[i][0]
-  }
-
-  return headers
-}
-*/

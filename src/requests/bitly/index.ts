20ca334a4e102780bb34202b8ffebab7ac810a46
Jesper Hägglund <jesper.hagglund@trmedia.se>
2020-04-20 11:32:53 +0200

    use axios bitly, disqus

--- src/requests/bitly/index.ts
+++ src/requests/bitly/index.ts
@@ -1,7 +1,8 @@
 import { endpoints } from './endpoints'
+import axios from 'axios'
 import { storeResponseMeta } from '../../data-access-layer/storeMeta'
 import { APIs } from '../../enums/APIs'
-import fetch from 'node-fetch'
+import extractRequestHeaders from '../../utils/extractRequestHeaders'
 const ACCESS_TOKEN = process.env.BITLY_ACCESS_TOKEN
 const BASE_URL = 'https://api-ssl.bitly.com/v4/'
 
@@ -14,42 +15,37 @@ interface Endpoint {
 export default async () => {
   const result = await Promise.all(
     endpoints.map(async (endpoint: Endpoint) => {
-      const res = await fetch(`${BASE_URL}${endpoint.url}`, {
-        method: endpoint.method,
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${ACCESS_TOKEN}`,
-        },
-        body:
-          endpoint.data && endpoint.method !== 'GET'
-            ? JSON.stringify(endpoint.data)
-            : undefined,
-      })
-      const body = await res.text()
-      const headers: any = {}
-      res.headers.forEach((value, name) => (headers[name] = value))
-      return {
-        wholeURI: res.url,
-        endpoint: endpoint.url,
-        statusCode: res.status,
-        headers,
-        body: JSON.parse(body),
-        httpMethod: endpoint.method,
+      try {
+        const res = await axios[endpoint.method](`${BASE_URL}${endpoint.url}`, {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+          },
+          data:
+            endpoint.data && endpoint.method !== 'GET'
+              ? JSON.stringify(endpoint.data)
+              : undefined,
+        })
+        const reqHeaderString = res.request._header
+        const reqHeaders = extractRequestHeaders(reqHeaderString)
+
+        storeResponseMeta({
+          api: APIs.bitly,
+          wholeURI: `${BASE_URL}${endpoint.url}`,
+          endpoint: endpoint.url,
+          status: {
+            statusCode: res.status,
+            statusText: res.statusText,
+          },
+          requestHeaders: reqHeaders,
+          responseHeaders: res.headers,
+          body: res.data,
+          httpMethod: endpoint.method,
+        })
+      } catch (err) {
+        console.log(err)
       }
     })
   )
-  // TODO argument should now be an object, see: src/interfaces/IResponseParams.ts
-
-  //result.map((res) =>
-  //   storeResponseMeta(
-  //     APIs.bitly,
-  //     res.wholeURI,
-  //     res.endpoint,
-  //     res.statusCode,
-  //      res.headers,
-  //  res.body,
-  //    res.httpMethod
-  // )
-  // )
 }
--- src/requests/disqus/index.ts
+++ src/requests/disqus/index.ts
@@ -1,7 +1,8 @@
 import { level1, level2 } from './endpoints'
+import axios from 'axios'
 import { storeResponseMeta } from '../../data-access-layer/storeMeta'
 import { APIs } from '../../enums/APIs'
-import fetch from 'node-fetch'
+import extractRequestHeaders from '../../utils/extractRequestHeaders'
 const DISQUS_SECRET = process.env.DISQUS_SECRET
 const DISQUS_ACCESS_TOKEN = process.env.DISQUS_ACCESS_TOKEN
 const BASE_URL = 'https://disqus.com/api/3.0/'
@@ -17,32 +18,40 @@ export default async () => {
   for (const level of [level1, level2]) {
     const result = await Promise.all(
       level.map(async (endpoint: Endpoint) => {
-        const res = await fetch(
-          `${BASE_URL}${
-            endpoint.url
-          }?api_secret=${DISQUS_SECRET}&access_token=${DISQUS_ACCESS_TOKEN}${
-            endpoint.params ? endpoint.params : ''
-          }`,
-          {
-            method: endpoint.method,
-            body:
-              endpoint.data && endpoint.method !== 'GET'
-                ? JSON.stringify(endpoint.data)
-                : undefined,
-          }
-        )
+        try {
+          const res = await axios[endpoint.method](
+            `${BASE_URL}${
+              endpoint.url
+            }?api_secret=${DISQUS_SECRET}&access_token=${DISQUS_ACCESS_TOKEN}${
+              endpoint.params ? endpoint.params : ''
+            }`,
+            {
+              method: endpoint.method,
+              data:
+                endpoint.data && endpoint.method !== 'get'
+                  ? JSON.stringify(endpoint.data)
+                  : undefined,
+            }
+          )
 
-        const body = await res.text()
-        const headers: any = {}
-        res.headers.forEach((value, name) => (headers[name] = value))
+          const reqHeaderString = res.request._header
+          const reqHeaders = extractRequestHeaders(reqHeaderString)
 
-        return {
-          wholeURI: res.url,
-          endpoint: endpoint.url,
-          statusCode: res.status,
-          headers,
-          body: JSON.parse(body),
-          httpMethod: endpoint.method,
+          storeResponseMeta({
+            api: APIs.disqus,
+            wholeURI: `${BASE_URL}${endpoint.url}`,
+            endpoint: endpoint.url,
+            status: {
+              statusCode: res.status,
+              statusText: res.statusText,
+            },
+            requestHeaders: reqHeaders,
+            responseHeaders: res.headers,
+            body: res.data,
+            httpMethod: endpoint.method,
+          })
+        } catch (err) {
+          console.log(err)
         }
       })
     )

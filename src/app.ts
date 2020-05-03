dd4059a600d66d085bb07ed0780fb64897e38deb
edvinpontuslarsson <el223na@student.lnu.se>
2020-05-03 14:15:02 +0200

    removes se from all

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ import fs from 'fs'
 
 dotenv.config()
 
-import { doStackExchangeRequests } from './requests/stackExchange/stackExchange'
+// TODO fix stack exchange
+// import { doStackExchangeRequests } from './requests/stackExchange/stackExchange'
 import doTwitterRequests from './requests/twitter'
 import bitlyRequests from './requests/bitly'
 import disqusRequests from './requests/disqus'
@@ -11,11 +12,11 @@ import imgurRequests from './requests/imgur'
 import writeURIToFile from './utils/writeURIToFile'
 
 const APIs: any = {
-  stackexchange: doStackExchangeRequests,
+  // stackexchange: doStackExchangeRequests,
   twitter: doTwitterRequests,
   bitly: bitlyRequests,
   disqus: disqusRequests,
-  imgur: imgurRequests
+  imgur: imgurRequests,
 }
 
 // TODO 1 table per api first

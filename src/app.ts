611e61db39e2adbff1d0720721b447b72e6e60c4
edvinpontuslarsson <el223na@student.lnu.se>
2020-05-10 10:48:02 +0200

    style changes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,5 @@
 import dotenv from 'dotenv'
 import fs from 'fs'
-
-dotenv.config()
-
 import stackExchangeRequests from './requests/stackExchange'
 import doTwitterRequests from './requests/twitter'
 import bitlyRequests from './requests/bitly'
@@ -10,6 +7,8 @@ import disqusRequests from './requests/disqus'
 import imgurRequests from './requests/imgur'
 import nasaRequests from './requests/nasa'
 import writeURIToFile from './utils/writeURIToFile'
+
+dotenv.config()
 
 const APIs: any = {
   stackexchange: stackExchangeRequests,
@@ -20,9 +19,7 @@ const APIs: any = {
   nasa: nasaRequests,
 }
 
-// TODO add responses.json as appendix
-// extra important since it can't be replicated
-// without needed keys
+// TODO add responses.json as appendix, extra important since it can't be replicated without needed keys
 
 const appArguments = process.argv.slice(2)
 
@@ -30,6 +27,7 @@ if (appArguments[0] === 'uris') {
   writeURIToFile()
 } else {
   fs.writeFileSync('responses.json', '[]')
+
   if (appArguments[0] === 'all') {
     Object.keys(APIs).forEach((api: string) => APIs[api]())
   } else if (appArguments.length === 0) {

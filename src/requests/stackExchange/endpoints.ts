7ad4687cbdd71c4a910f2e3ef60c96fcc795058e
edvinpontuslarsson <el223na@student.lnu.se>
2020-05-11 10:11:16 +0200

    auto se req among all

--- src/requests/stackExchange/endpoints.ts
+++ src/requests/stackExchange/endpoints.ts
@@ -9,6 +9,12 @@ const endpoints: ISEEndpoint[] = [
   },
   {
     method: HTTPMethods.GET,
+    /**
+     * Related questions to these 3 questions:
+     * https://stackoverflow.com/questions/60075228/exchange-different-arrays-elements-in-ruby
+     * https://stackoverflow.com/questions/60075237/best-way-to-handle-an-account-linking-verification-system
+     * https://stackoverflow.com/questions/57496313/execution-failed-for-task-appmergedebugresources-com-android-builder-interna
+     */
     url:
       'questions/60075228;60075237;57496313/related?order=desc&sort=activity&site=stackoverflow',
     endpoint: 'questions/{ids}/related?order=desc&sort=activity&site={site}',
--- src/requests/stackExchange/index.ts
+++ src/requests/stackExchange/index.ts
@@ -1,74 +1,44 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { storeResponseMeta } from '../../data-access-layer/storeMeta'
 import { APIs } from '../../enums/APIs'
-import { GET, POST, PUT, PATCH, DELETE } from '../../utils/HTTPMethods'
+import { HTTPMethods } from '../../enums/HTTPMethods'
 import extractRequestHeaders from '../../utils/extractRequestHeaders'
 import endpoints from './endpoints'
 
 export default (): void => {
-  endpoints.forEach((ep) => {
-    const url = ep.url
+  endpoints.forEach(async (ep) => {
+    const fullUri = `https://api.stackexchange.com/2.2/${ep.url}`
+
+    if (ep.method === HTTPMethods.GET) {
+      const res = await axiosGETRequest(fullUri)
+
+      const reqHeaderString = res.request._header
+      const reqHeaders = extractRequestHeaders(reqHeaderString)
+
+      storeResponseMeta({
+        api: APIs.stackExchange,
+        wholeURI: fullUri,
+        endpoint: ep.endpoint ? ep.endpoint : ep.url,
+
+        status: {
+          statusCode: res.status,
+          statusText: res.statusText,
+        },
+
+        requestHeaders: reqHeaders,
+        responseHeaders: res.headers,
+
+        body: res.data,
+        httpMethod: ep.method,
+      })
+    }
   })
 }
 
-function stackOverflowInfo() {
-  const uri = 'https://api.stackexchange.com/2.2/info?site=stackoverflow'
-
-  // TODO for each url, check valid
-
-  axios.get(uri).then((response) => {
-    const reqHeaderString = response.request._header
-    const reqHeaders = extractRequestHeaders(reqHeaderString)
-
-    storeResponseMeta({
-      api: APIs.stackExchange,
-      endpoint: '/2.2/info?site=stackoverflow',
-      wholeURI: uri,
-
-      httpMethod: GET,
-      status: {
-        statusCode: response.status,
-        statusText: response.statusText,
-      },
-
-      requestHeaders: reqHeaders,
-      responseHeaders: response.headers,
-
-      body: response.data,
-    })
-  })
-}
-
-/**
- * Related questions to these 3 questions:
- * https://stackoverflow.com/questions/60075228/exchange-different-arrays-elements-in-ruby
- * https://stackoverflow.com/questions/60075237/best-way-to-handle-an-account-linking-verification-system
- * https://stackoverflow.com/questions/57496313/execution-failed-for-task-appmergedebugresources-com-android-builder-interna
- */
-function relatedQuestionsSO() {
-  const uri =
-    'https://api.stackexchange.com/2.2/questions/60075228;60075237;57496313/related?order=desc&sort=activity&site=stackoverflow'
-
-  axios.get(uri).then((response) => {
-    const reqHeaderString = response.request._header
-    const reqHeaders = extractRequestHeaders(reqHeaderString)
-
-    storeResponseMeta({
-      api: APIs.stackExchange,
-      endpoint:
-        '/2.2/questions/60075228;60075237;57496313/related?order=desc&sort=activity&site=stackoverflow',
-      wholeURI: uri,
-
-      httpMethod: GET,
-      status: {
-        statusCode: response.status,
-        statusText: response.statusText,
-      },
-
-      requestHeaders: reqHeaders,
-      responseHeaders: response.headers,
-
-      body: response.data,
+function axiosGETRequest(url: string): Promise<AxiosResponse<any>> {
+  return new Promise((resolve) => {
+    axios.get(url).then((res) => {
+      resolve(res)
     })
   })
 }

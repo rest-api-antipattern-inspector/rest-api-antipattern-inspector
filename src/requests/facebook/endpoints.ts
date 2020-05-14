ec9152ac09bded736c870fc69fcef73a7a24df31
edvinpontuslarsson <el223na@student.lnu.se>
2020-05-14 14:55:46 +0200

    accounts & albums work

--- src/requests/facebook/endpoints.ts
+++ src/requests/facebook/endpoints.ts
@@ -5,8 +5,18 @@ const userID = process.env.FACEBOOK_USER_ID
 
 const endpoints: IFBEndpoint[] = [
   {
-    endpoint: 'person-id',
+    endpoint: '{person-id}',
     url: userID,
+    method: HTTPMethods.GET,
+  },
+  {
+    endpoint: '{user-id}/accounts',
+    url: `${userID}/accounts`,
+    method: HTTPMethods.GET,
+  },
+  {
+    endpoint: '{user-id}/albums',
+    url: `${userID}/albums`,
     method: HTTPMethods.GET,
   },
 ]
